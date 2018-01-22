62d4c89485585e2cf85a2558c1227a038cf17546
LCJ-MinYa <1049468118@qq.com>
2018-01-22 10:16:11 +0800

    yarn命令记录

---
+++ JS知识点/yarn命令.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+ * ===========================
+ *
+ * yarn命令
+ * @author   : LiChaoJun
+ * @datetime : 2018/01/22
+ *
+ * ===========================
+ */
+
+console.log(`
+与NPM命令对照:
+npm install  => yarn install
+npm install --save [package] => yarn add [package]
+npm install --save-dev [package] => yarn add [package] --dev
+npm install --global [package] => yarn global add [package]
+npm uninstall --save [package] => yarn remove [package]
+npm uninstall --save-dev [package] => yarn remove [package]
+`);
